d94df7bab8a063f1e36562a651c494d051b7c02f
lovehacker <61407838+lovehackerAsif@users.noreply.github.com>
2020-04-22 11:24:36 +0500

    Update lov3.py

--- lov3.py
+++ lov3.py
@@ -283,7 +283,7 @@ def login():
 	        print "\033[1;95m•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬ •◈•\033[1;91mBlackMafia\033[1;95m•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•"
 	        print('	   \033[1;91m▬\x1b[1;95m.........LOGIN WITH FACEBOOK........\x1b[1;91m▬' )
 	        print('	' )
-	        id = raw_input('\033[1;91m[+] \x1b[1;91mID/Email\x1b[1;95m: \x1b[1;95m')
+	        id = raw_input('\033[1;91m[+] \x1b[1;91mID/login facebook\x1b[1;95m: \x1b[1;95m')
 	        pwd = raw_input('\033[1;91m[+] \x1b[1;91mPassword\x1b[1;95m: \x1b[1;95m')
 	        tik()
 		try:
@@ -328,6 +328,26 @@ def login():
 			os.system('rm -rf login.txt')
 			time.sleep(1)
 			login()
+def tokenz():
+	os.system('clear')
+	toket = raw_input("\033[1;91m[?] \033[1;92mToken\033[1;91m : \033[1;97m")
+	try:
+		otw = requests.get('https://graph.facebook.com/me?access_token='+toket)
+		a = json.loads(otw.text)
+		nama = a['name']
+		zedd = open("login.txt", 'w')
+		zedd.write(toket)
+		zedd.close()
+		menu()
+	except KeyError:
+		print "\033[1;91m[!] Wrong"
+		e = raw_input("\033[1;91m[?] \033[1;92mWant to pick up token?\033[1;97m[y/n]: ")
+		if e =="":
+			keluar()
+		elif e =="y":
+			login()
+		else:
+			keluar()
 
 
 def menu():
